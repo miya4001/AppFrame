ff96a18abc88296bba9e6bbc405e41fd12993326
宮澤耀生 <121pg0023@tokyo.amg.ac.jp>
2022-04-24 16:58:11 +0900

    Application:アプリケーションの基底クラス

---
+++ source/Application/ApplicationBase.cpp
@@ -0,0 +1,152 @@
+/*****************************************************************//**
+ * @file   ApplicationBase.cpp
+ * @brief  アプリケーションの基底クラス
+ * 
+ * @author 宮澤耀生
+ * @date   April 2022
+ *********************************************************************/
+#include "ApplicationBase.h"
+#include <DxLib.h>
+#include <EffekseerForDXLib.h>
+#include <stdexcept>
+
+namespace {
+  constexpr int Error = -1;  //!< エラー
+} // namespace
+
+/**
+ * @brief  アプリケーションフレーム
+ */
+namespace AppFrame {
+  namespace Application {
+    ApplicationBase::ApplicationBase() {
+      // 画面モード切り替え
+#ifdef _DEBUG
+      _windowMode = true;   //ウィンドウ
+#else
+      _windowMode = false;  // フルスクリーン
+#endif
+    }
+
+    ApplicationBase::~ApplicationBase() {
+
+    }
+
+    bool ApplicationBase::Init() {
+      // DXライブラリの初期化
+      if (!DxLibInit()) {
+        return false;  // 初期化失敗
+      }
+      //Effekseerの初期化
+      if (!EffekseerInit()) {
+        return false;  // 初期化失敗
+      }
+      return true;  // 初期化成功
+    }
+
+    void ApplicationBase::Run() {
+      // メインループ
+      while (ProcessMessage() != Error) {
+        // アプリケーションを終了する場合
+        if (_appState == AppState::Quit) {
+          break;  // ループを抜ける
+        }
+#ifdef _DEBUG
+        try {
+          Input();    // 入力
+          Process();  // 更新
+          Draw();     // 描画
+        }
+        catch (std::logic_error error) {
+          // 例外発生時、出力を行う
+          OutputDebugString(error.what());
+        }
+#else
+        Input();    // 入力
+        Process();  // 更新
+        Draw();     // 描画
+#endif
+      }
+    }
+
+    void ApplicationBase::Terminate() {
+      // Effekseerの終了
+      Effkseer_End();
+      // DXライブラリの終了
+      DxLib_End();
+    }
+
+    void ApplicationBase::RequestTerminate() {
+      // アプリケーションが終了状態ではない場合
+      if (_appState != AppState::Quit) {
+        _appState = AppState::Quit;  // 終了
+      }
+    }
+
+    void ApplicationBase::Input() {
+
+    }
+
+    void ApplicationBase::Process() {
+
+    }
+
+    void ApplicationBase::Draw() {
+      // 画面をクリア
+      ClearDrawScreen();
+      // 裏画面の内容を表画面に反映
+      ScreenFlip();
+    }
+
+    bool ApplicationBase::DxLibInit() {
+      // ログファイルの出力設定
+      SetOutApplicationLogValidFlag(_windowMode);
+      // 画面モードの変更
+      SetGraphMode(ScreenWidth, ScreenHeight, ColorBit);
+      ChangeWindowMode(_windowMode);
+      // DXライブラリの初期化
+      if (DxLib_Init() == Error) {
+#ifdef _DEBUG
+        throw std::logic_error("ApplicationBase:DXライブラリの初期化に失敗しました\n");
+#endif
+        return false;  // 初期化失敗
+      }
+      // 裏面描画の設定
+      SetDrawScreen(DX_SCREEN_BACK);
+      // 背景色の設定
+      SetBackgroundColor(0, 0, 255);
+      // Zバッファの設定
+      SetUseZBuffer3D(true);    // 使用する
+      SetWriteZBuffer3D(true);  // 書き込みを行う
+      return true;  // 初期化成功
+    }
+
+    bool ApplicationBase::EffekseerInit() {
+      // DirectX11を使用
+      SetUseDirect3DVersion(DX_DIRECT3D_11);
+      // Effekseerの初期化
+      if (Effekseer_Init(Particle) == Error) {
+#ifdef _DEBUG
+        throw std::logic_error("ApplicationBase:Effekseerの初期化に失敗しました\n");
+#endif
+        return false;  // 初期化失敗
+      }
+      // フルスクリーンウインドウの切り替えでリソースが消えるのを防ぐ
+      SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
+      // DXライブラリのデバイスロストした時のコールバックを設定
+      Effekseer_SetGraphicsDeviceLostCallbackFunctions();
+      return true;  // 初期化成功
+    }
+
+    bool ApplicationBase::IsSetApplication() {
+      // アプリケーション未生成
+      if (!_isSetApp) {
+        if (_application == nullptr) {
+          _isSetApp = true;
+          return true;  // 生成する
+        }
+      }
+      return false;  // 生成しない
+    }
+  } // namespace Application
+} // namespace AppFrame
---
+++ source/Application/ApplicationBase.h
@@ -0,0 +1,112 @@
+/*****************************************************************//**
+ * @file   ApplicationBase.h
+ * @brief  アプリケーションの基底クラス
+ * 
+ * @author 宮澤耀生
+ * @date   April 2022
+ *********************************************************************/
+#pragma once
+#include <memory>
+
+ /**
+  * @brief  アプリケーションフレーム
+  */
+namespace AppFrame {
+  constexpr int ScreenWidth = 1920;   //!< 画面解像度(横)
+  constexpr int ScreenHeight = 1080;  //!< 画面解像度(縦)
+  constexpr int ColorBit = 32;        //!< ビットカラー
+  constexpr int Particle = 8000;      //!< パーティクル数
+  /**
+   * @brief  アプリケーション
+   */
+  namespace Application {
+    /**
+     * @class  ApplicationBase
+     * @brief  アプリケーションの基底クラス
+     */
+    class ApplicationBase {
+    public:
+      /**
+       * @brief  アプリケーションの状態
+       */
+      enum class AppState {
+        Play,    //!< 実行中
+        Paused,  //!< 一時停止
+        Quit     //!< 終了
+      };
+      /**
+       * @brief  コンストラクタ
+       */
+      ApplicationBase();
+      /**
+       * @brief  デストラクタ
+       */
+      virtual ~ApplicationBase();
+      /**
+       * @brief  アプリケーションの初期化
+       * @return true:初期化成功
+       *         false:初期化失敗
+       */
+      virtual bool Init();
+      /**
+       * @brief  アプリケーションの実行
+       */
+      virtual void Run();
+      /**
+       * @brief  アプリケーションの終了
+       */
+      virtual void Terminate();
+      /**
+       * @brief  アプリケーションの終了要請
+       */
+      virtual void RequestTerminate();
+      /**
+       * @brief  アプリケーションの参照を取得
+       * @return アプリケーションの参照
+       */
+      static std::shared_ptr<ApplicationBase> GetApplication() {
+        return _application;
+      }
+
+    protected:
+      /**
+       * @brief  入力
+       */
+      virtual void Input();
+      /**
+       * @brief  更新
+       */
+      virtual void Process();
+      /**
+       * @brief  描画
+       */
+      virtual void Draw();
+      /**
+       * @brief  Dxライブラリの初期化
+       * @return true:初期化成功
+       *         false:初期化失敗
+       */
+      virtual bool DxLibInit();
+      /**
+       * @brief  Effekseerの初期化
+       * @return true:初期化成功
+       *         false:初期化失敗
+       */
+      virtual bool EffekseerInit();
+      /**
+       * @brief  アプリケーションを生成するかの判定
+       * @return true:生成する
+       *         false:生成しない
+       */
+      static bool IsSetApplication();
+
+      static inline bool _windowMode{ false };  //!< 画面モード
+      static inline bool _isSetApp{ false };    //!< 生成フラグ
+
+      //!< アプリケーションの状態
+      AppState _appState{ AppState::Paused };
+      //!< アプリケーションの実態
+      static inline std::shared_ptr<ApplicationBase> _application{ nullptr };
+    };
+  } // namespace Application
+} // namespace AppFrame
