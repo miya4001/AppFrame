4731417ff85e63a7faaa27ac6c94b77ada0de897
宮澤耀生 <121pg0023@tokyo.amg.ac.jp>
2022-04-25 21:57:23 +0900

    Mode:モードの基底クラス

---
+++ source/Mode/ModeBase.cpp
@@ -0,0 +1,48 @@
+/*****************************************************************//**
+ * @file   ModeBase.cpp
+ * @brief  ÉÇÅ[ÉhÇÃäÓíÍÉNÉâÉX
+ * 
+ * @author ã{‡Vósê∂
+ * @date   April 2022
+ *********************************************************************/
+#include "ModeBase.h"
+
+namespace AppFrame {
+  namespace Mode {
+    ModeBase::ModeBase(Application::ApplicationBase& app) : _app(app) {
+
+    }
+
+    ModeBase::~ModeBase() {
+
+    }
+
+    bool ModeBase::Init() {
+      return true;
+    }
+
+    void ModeBase::Enter() {
+
+    }
+
+    void ModeBase::Exit() {
+
+    }
+
+    void ModeBase::Input(Input::InputManager& input) {
+
+    }
+
+    void ModeBase::Process() {
+
+    }
+
+    void ModeBase::Draw() const {
+
+    }
+
+    Application::ApplicationBase& ModeBase::GetApplication() {
+      return _app;
+    }
+  } // namespace Mode
+} // namespace AppFrame
---
+++ source/Mode/ModeBase.h
@@ -0,0 +1,85 @@
+/*****************************************************************//**
+ * @file   ModeBase.h
+ * @brief  モードの基底クラス
+ * 
+ * @author 宮澤耀生
+ * @date   April 2022
+ *********************************************************************/
+#pragma once
+
+/**
+ * @brief  アプリケーションフレーム
+ */
+namespace AppFrame {
+  /**
+   * @brief  アプリケーション
+   */
+  namespace Application {
+    class ApplicationBase;
+  } // namespace Application
+  /**
+   * @brief  入力
+   */
+  namespace Input {
+    class InputManager;
+  } // namespace Input
+  /**
+   * @brief  モード
+   */
+  namespace Mode {
+    /**
+     * @class  ModeBase
+     * @brief  モードの基底クラス
+     */
+    class ModeBase {
+    public:
+      /**
+       * @brief  コンストラクタ
+       * @param  app アプリケーションの参照
+       */
+      ModeBase(Application::ApplicationBase& app);
+      /**
+       * @brief  デストラクタ
+       */
+      virtual ~ModeBase();
+      /**
+       * @brief  初期化
+       * @return true:初期化成功
+       *         false:初期化失敗
+       */
+      virtual bool Init();
+      /**
+       * @brief  モード開始
+       */
+      virtual void Enter();
+      /**
+       * @brief  モード終了
+       */
+      virtual void Exit();
+      /**
+       * @brief  入力
+       * @param  input インプットマネージャーの参照
+       */
+      virtual void Input(Input::InputManager& input);
+      /**
+       * @brief  更新
+       */
+      virtual void Process();
+      /**
+       * @brief  描画
+       */
+      virtual void Draw() const;
+      /**
+       * @brief  アプリケーションの取得
+       * @return アプリケーションの参照
+       */
+      virtual Application::ApplicationBase& GetApplication();
+
+    protected:
+      int _modeCount{ 0 };    //!< 本モード内経過時間(フレーム)
+      bool _isLoad{ false };  //!< リソース読み込みフラグ
+      //!< アプリケーションの参照
+      Application::ApplicationBase& _app;
+    };
+  } // namespace Mode
+} // namespace AppFrame
